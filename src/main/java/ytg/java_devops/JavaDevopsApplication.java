82e420053528a1c4c35c2cfc27dea0c35dcd1cc9
Yassine LAHMAR-CHERIF <72023637+YassineLC@users.noreply.github.com>
2026-03-27 11:54:24 +0100

    Initial commit: Spring Boot hello-world REST service with Dockerfile

---
+++ src/main/java/ytg/java_devops/JavaDevopsApplication.java
@@ -0,0 +1,11 @@
+package ytg.java_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevopsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevopsApplication.class, args);
+    }
+}
